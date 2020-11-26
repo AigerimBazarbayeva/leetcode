f2550e4f78a4d541708b2f0ddced2da5625ff3fd
AigerimBazarbayeva <ai.bazarbayeva@gmail.com>
2020-11-26 13:21:52 +0100

    1662. Check If Two String Arrays are Equivalent

---
+++ src/ArrayStringsAreEqual.java
@@ -0,0 +1,25 @@
+class ArrayStringsAreEqual {
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 = "";
+        String s2 = "";
+
+        for(String s: word1){
+            s1 += s;
+        }
+
+        for(String s: word2){
+            s2 += s;
+        }
+
+        if(s1.equals(s2)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(arrayStringsAreEqual(new String[]{"a", "bc"}, new String[]{"ab", "c"}));
+        System.out.println(arrayStringsAreEqual(new String[]{"aa", "bc"}, new String[]{"ab", "c"}));
+    }
+}
