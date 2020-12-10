7cfec87da5dc858e221c25fc9868afde5b2d09b4
AigerimBazarbayeva <ai.bazarbayeva@gmail.com>
2020-12-10 23:41:33 +0100

    700. Search in a Binary Search Tree

---
+++ src/SearchInABinaryTree.java
@@ -0,0 +1,45 @@
+public class SearchInABinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    TreeNode res = new TreeNode();
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if(root != null){
+            traverse(root, val);
+        }
+
+        if(res.val != val){
+            return null;
+        }
+
+        return res;
+    }
+
+    private void traverse(TreeNode node, int val){
+
+        if( node != null && node.val == val){
+            res = node;
+        }
+
+        if(node.left != null){
+            traverse(node.left, val);
+        }
+
+        if(node.right != null){
+            traverse(node.right, val);
+        }
+
+    }
+}
